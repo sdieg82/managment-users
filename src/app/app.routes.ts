4196938475cb4c1f8a2984faac91169e98ffc2c3
Diego <sdieg82@gmail.com>
2024-11-28 09:37:40 -0500

    structure folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './users/pages/main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'mainUser',
+        component: MainPageComponent
+    }
+];
---
+++ src/app/users/pages/products-page/products-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-page.component.html',
+  styleUrl: './products-page.component.css'
+})
+export class ProductsPageComponent {
+
+}
